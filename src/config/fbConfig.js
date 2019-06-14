4e2b8e152e13b8b06a9ad4ca0f98480a59416621
obloow <louismarie.merminod@gmail.com>
2019-06-14 20:30:00 +0200

    first save

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Replace this with your own config details
+const config = {
+  apiKey: "AIzaSyDCZmhwLCcv9HfhyrQwUJ4TwFG1BJy-Q6Y",
+  authDomain: "obloow-marioplan-c2162.firebaseapp.com",
+  databaseURL: "https://obloow-marioplan-c2162.firebaseio.com",
+  projectId: "obloow-marioplan-c2162",
+  storageBucket: "obloow-marioplan-c2162.appspot.com",
+  messagingSenderId: "495635202536",
+  appId: "1:495635202536:web:5a56ac46c180e881"
+};
+firebase.initializeApp(config);
+
+export default firebase 
