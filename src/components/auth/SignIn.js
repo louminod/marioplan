4e18df6b1105d2328b8aece7e2a5b9c0e01cbe16
obloow <obloow.user@gmail.com>
2019-06-15 11:59:16 +0200

    route guarding

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { signIn } from '../../store/actions/authActions'
+import { Redirect } from 'react-router-dom'
 
 class SignIn extends Component {
   state = {
@@ -17,7 +18,8 @@ class SignIn extends Component {
     this.props.signIn(this.state)
   }
   render() {
-    const { authError } = this.props;
+    const { authError, auth } = this.props;
+    if (auth.uid) return <Redirect to='/' />
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -33,7 +35,7 @@ class SignIn extends Component {
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Login</button>
             <div className="center red-text">
-              { authError ? <p>{authError}</p> : null }
+              {authError ? <p>{authError}</p> : null}
             </div>
           </div>
         </form>
@@ -43,8 +45,9 @@ class SignIn extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return{
-    authError: state.auth.authError
+  return {
+    authError: state.auth.authError,
+    auth: state.firebase.auth
   }
 }
 
