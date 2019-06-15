4dfb85162b62ec1659dbe1ca007b2597d1030acf
obloow <obloow.user@gmail.com>
2019-06-15 22:26:06 +0200

    ajout de fonctions

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
+import moment from 'moment'
 
 const ProjectDetails = (props) => {
   const { project, auth } = props;
@@ -13,11 +14,11 @@ const ProjectDetails = (props) => {
         <div className="card z-depth-0">
           <div className="card-content">
             <span className="card-title">{project.title}</span>
-            <p>{project.content}</p>s
+            <p>{project.content}</p>
           </div>
           <div className="card-action grey lighten-4 grey-text">
             <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
-            <div>2nd September, 2am</div>
+            <div>{moment(project.createdAt.toDate()).calendar()}</div>
           </div>
         </div>
       </div>
